1bc6d974971fbd535a4eaf6c165ac4d4f451fb17
l.murillo.baltodano <l.murillo.baltodano@accenture.com>
2021-11-25 23:14:16 -0600

    enhance images

--- scenes/Default/linkImages.store.ts
+++ scenes/Default/linkImages.store.ts
@@ -1,34 +1,30 @@
 export default {
   Chubascos: [
     "https://chubascos.netlify.com/",
-    "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/127577/nature-stars-night-galaxy-127577.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   ],
   Wattpad: [
     "https://www.wattpad.com/user/Odoriens",
-    "https://images.pexels.com/photos/1179229/pexels-photo-1179229.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1274260/pexels-photo-1274260.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   ],
   Instagram: [
     "https://www.instagram.com/latestcoder/",
-    "https://images.pexels.com/photos/1563604/pexels-photo-1563604.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/2469122/pexels-photo-2469122.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   ],
-  "NPM Scene Preset": [
+  "Scene Preset": [
     "https://www.npmjs.com/package/scene-preset",
-    "https://images.pexels.com/photos/1834399/pexels-photo-1834399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  ],
-  "NPM Canvas Preset": [
-    "https://www.npmjs.com/package/canvas-preset",
-    "https://images.pexels.com/photos/2876511/pexels-photo-2876511.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/1376766/nature-milky-way-galaxy-space-1376766.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   ],
   Codepen: [
     "https://codepen.io/LuisArmando-TC",
-    "https://images.pexels.com/photos/1459534/pexels-photo-1459534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/544268/pexels-photo-544268.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   ],
   Portfolio: [
     "http://testcoder.netlify.com/",
-    "https://images.pexels.com/photos/131723/pexels-photo-131723.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/1830252/pexels-photo-1830252.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   ],
   Shadertoy: [
     "https://www.shadertoy.com/user/TestCoder",
-    "https://images.pexels.com/photos/3408552/pexels-photo-3408552.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/2347011/pexels-photo-2347011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   ],
 } as { [index: string]: [string, string] };
