496f8aa4292aeb8f3e44bd5af45c6cc7ace6decd
l.murillo.baltodano <l.murillo.baltodano@accenture.com>
2021-11-25 20:52:16 -0600

    Add py autos

---
+++ scene.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from modules.main import executeConditionalPath, createFile
+
+sceneName = input("Scene name: ")
+folderPath = f"../scenes/{sceneName}"
+
+executeConditionalPath(
+    folderPath,
+    lambda path: os.makedirs(path)
+)
+
+executeConditionalPath(
+    f"{folderPath}/index.ts",
+    lambda path: createFile(
+        path,
+        "import * as THREE from 'three'\n\n" +
+
+        "import presetScene, { actions } from 'scene-preset'\n\n" +
+
+        "export default id => presetScene({\n" +
+            "\tsetup({ scene }) {},\n" +
+            "\tanimate({ scene }) {}\n" +
+        "}, `#${id}`)"
+    )
+)
