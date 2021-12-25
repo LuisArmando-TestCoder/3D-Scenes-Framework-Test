f4ea816375105d2332f40b069b3d11132d68c3f5
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2021-12-24 19:42:52 -0600

    Add eating behavior

--- scenes/Animation/scene.ts
+++ scenes/Animation/scene.ts
@@ -7,8 +7,18 @@ import Text from "../../meshes/Text";
 import Model from "../../meshes/Model";
 import stage from "../../stages/nightSkyReflectors";
 
+const randomBreakpoints = getRandomBreakpoints(10, 45);
+
 export default {
   ...stage,
+  breakpoints: {
+    properties: {
+      position: {
+        y: -1.5,
+      },
+    },
+    object: () => randomBreakpoints,
+  } as unknown as Scene,
   character: {
     properties: {
       position: {
@@ -23,8 +33,8 @@ export default {
       { object3D, animations }: SceneExport,
       canvasState: types.state.CanvasState
     ) {
-      approachTarget({
-        target: canvasState.camera?.position as THREE.Vector3,
+      pursueBreakpoints({
+        breakpoints: randomBreakpoints,
         object3D,
         animations,
       });
@@ -32,11 +42,85 @@ export default {
   } as unknown as Scene,
 } as Scenes;
 
+// Crea una carrera de obstáculos donde cambias el target para que el fox siga los breakpoints
+// Crea o busca una función que genera desplazamientos 2D en bezier curbes
+// Make the fox jump
+function getRandomBreakpoints(
+  amount: number,
+  distance: number,
+  mesh: THREE.Mesh = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.25, 100, 100),
+    new THREE.MeshBasicMaterial({
+      color: "#f00",
+    })
+  )
+) {
+  return [...new Array(amount)].map(() => {
+    const step = Math.random() * Math.PI * 2;
+    const randomDistance = distance * Math.random();
+    const clone = mesh.clone();
+
+    clone.position.x = Math.sin(step) * randomDistance;
+    clone.position.z = Math.cos(step) * randomDistance;
+
+    return clone;
+  });
+}
+
+function distanceBetween(vector1: THREE.Vector3, vector2: THREE.Vector3) {
+  return new Victor(vector1.x, vector1.z).distance(
+    new Victor(vector2.x, vector2.z)
+  );
+}
+
+function pursueBreakpoints({
+  breakpoints,
+  object3D,
+  animations,
+  minimumDistance = 1,
+  speedScale = 0.01,
+}: {
+  breakpoints: THREE.Object3D[];
+  object3D: THREE.Object3D;
+  animations: Map<string, (number: number) => THREE.AnimationAction>;
+  minimumDistance?: number;
+  speedScale?: number;
+}) {
+  let closestVisibleBreakpoint: THREE.Object3D | null = null;
+  let lastCheckedDistance = Infinity;
+
+  for (const breakpoint of breakpoints) {
+    const distance = distanceBetween(object3D.position, breakpoint.position);
+
+    if (breakpoint.visible && distance < lastCheckedDistance) {
+      closestVisibleBreakpoint = breakpoint;
+      lastCheckedDistance = distance;
+    }
+  }
+
+  if (closestVisibleBreakpoint) {
+    if (
+      distanceBetween(closestVisibleBreakpoint.position, object3D.position) <=
+      minimumDistance
+    ) {
+      closestVisibleBreakpoint.visible = false;
+    }
+
+    approachTarget({
+      target: closestVisibleBreakpoint.position,
+      object3D,
+      animations,
+      speedScale,
+      minimumDistance,
+    });
+  }
+}
+
 function approachTarget({
   target,
   object3D,
   animations,
-  minimumDistance = 7,
+  minimumDistance = 2,
   speedScale = 0.01,
 }: {
   target: THREE.Vector3;
@@ -45,13 +129,10 @@ function approachTarget({
   minimumDistance?: number;
   speedScale?: number;
 }) {
-  const run = animations?.get?.("Run")?.(speedScale);
+  const distance = distanceBetween(target, object3D.position);
+  const run = animations?.get?.("Run")?.((distance * speedScale) / 10);
 
-  if (
-    new Victor(target.x, target.z).distance(
-      new Victor(object3D.position.x, object3D.position.z)
-    ) >= minimumDistance
-  ) {
+  if (distance >= minimumDistance) {
     object3D.lookAt(target.x, -1.5, target.z);
 
     object3D.position.x += speedScale * (target.x - object3D.position.x);
